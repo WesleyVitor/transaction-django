30526815a30dd8fe80334d64f00b40e35df1e498
WesleyVitor <wesleydemorais@outlook.com.br>
2022-08-02 12:56:21 -0300

    feat: Using transaction to save user

--- core/services/ordinary_profile.py
+++ core/services/ordinary_profile.py
@@ -1,10 +1,10 @@
-
+from django.db import transaction
 from core.models import CustomUser, Ordinary
 from core.serializers import InputOrdinaryProfileSerializer, InputCustomUserSerializer
 
 
 class OrdinaryProfileService:
-
+    @transaction.atomic
     def create_ordinary_profile(self,ordinary_serializer:InputOrdinaryProfileSerializer,user_serializer:InputCustomUserSerializer ):
         user:CustomUser = CustomUser.objects.create_user(email=user_serializer['email'].value, password=user_serializer['password1'].value)
         user.save()
